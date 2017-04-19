2dbec2697abdec1691c93207aa43b7d354773f13
SusanZheng15 <zheng.susan12@gmail.com>
2017-04-19 15:46:39 -0400

    added 2 arrays of language code and title, added pickerview delegate methods

--- TextToSpeech/ViewController.swift
+++ TextToSpeech/ViewController.swift
@@ -9,14 +9,19 @@
 import UIKit
 import AVFoundation
 
-class ViewController: UIViewController, AVSpeechSynthesizerDelegate, UITextFieldDelegate
+class ViewController: UIViewController, AVSpeechSynthesizerDelegate, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource
 {
     @IBOutlet weak var speechTextBar: UITextField!
-    
     @IBOutlet weak var accentPickerView: UIPickerView!
+    @IBOutlet weak var languageAccentLabel: UILabel!
     
     let speechSynthesizer = AVSpeechSynthesizer() //set the synthesizer
-    var speechVoice : AVSpeechSynthesisVoice? //set the voice to nil if there is none or default, idk
+    var speechVoice : AVSpeechSynthesisVoice? //set the voice to nil if there is none or default
+    
+    
+    var languageTitle : [String] = ["Arabic (Saudi Arabia)", "Chinese (China)", "Chinese (Hong Kong)", "Chinese (Taiwan)", "Czech (Czech Republic)", "Danish (Denmark)", "Dutch (Belgium)", "Dutch (Netherlands)", "English (Australia)", "English (Ireland)", "English (South Africa)", "English (United Kingdom)", "English (United States)", "Finnish (Finland)", "French (Canada)", "French (France)", "German (Germany)", "Greek (Greece)", "Hebrew (Israel)", "Hindi (India)", "Hungarian (Hungary)", "Indonesian (Indonesia)", "Italian (Italy)", "Japanese (Japan)", "Korean (South Korea)", "Norwegian (Norway)", "Polish (Poland)", "Portuguese (Brazil)", "Portuguese (Brazil)", "Romanian (Romania)", "Russian (Russia)", "Slovak (Slovakia)", "Spanish (Mexico)", "Spanish (Spain)", "Swedish (Sweden)", "Thai (Thailand)", "Turkish (Turkey)"]
+    
+    var languageAccentCode : [String] = ["ar-SA", "zh-CN", "zh-HK", "zh-TW", "cs-CZ", "da-DK", "nl-BE", "nl-NL", "en-AU", "en-IE", "en-ZA", "en-GB", "en-US", "fi-FI", "fr-CA", "fr-FR", "de-DE", "el-GR", "he-IL", "hi-IN", "hu-HU", "id-ID", "it-IT", "ja-JP", "ko-KR", "no-NO", "pl-PL", "pt-BR", "pt-PT", "ro-RO", "ru-RU", "sk-SK", "es-MX", "es-ES", "sv-SE", "th-TH", "tr-TR"]
     
     override func viewDidLoad()
     {
@@ -63,6 +68,32 @@ class ViewController: UIViewController, AVSpeechSynthesizerDelegate, UITextField
     {
         print("Finished saying '\(utterance.speechString)");
     }
+    
+    
+    
+    //UIPickerview delegate methods
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int
+    {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return languageTitle.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        return languageTitle[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        languageAccentLabel.text = languageTitle[row]
+    }
+    
     
     
     
