74032a7e284aaf0588b89dfedb6a3bb5df3ce0d3
SusanZheng15 <zheng.susan12@gmail.com>
2017-04-19 15:58:15 -0400

    accent changes based on user's pick on picker view

--- TextToSpeech/ViewController.swift
+++ TextToSpeech/ViewController.swift
@@ -18,7 +18,6 @@ class ViewController: UIViewController, AVSpeechSynthesizerDelegate, UITextField
     let speechSynthesizer = AVSpeechSynthesizer() //set the synthesizer
     var speechVoice : AVSpeechSynthesisVoice? //set the voice to nil if there is none or default
     
-    
     var languageTitle : [String] = ["Arabic (Saudi Arabia)", "Chinese (China)", "Chinese (Hong Kong)", "Chinese (Taiwan)", "Czech (Czech Republic)", "Danish (Denmark)", "Dutch (Belgium)", "Dutch (Netherlands)", "English (Australia)", "English (Ireland)", "English (South Africa)", "English (United Kingdom)", "English (United States)", "Finnish (Finland)", "French (Canada)", "French (France)", "German (Germany)", "Greek (Greece)", "Hebrew (Israel)", "Hindi (India)", "Hungarian (Hungary)", "Indonesian (Indonesia)", "Italian (Italy)", "Japanese (Japan)", "Korean (South Korea)", "Norwegian (Norway)", "Polish (Poland)", "Portuguese (Brazil)", "Portuguese (Brazil)", "Romanian (Romania)", "Russian (Russia)", "Slovak (Slovakia)", "Spanish (Mexico)", "Spanish (Spain)", "Swedish (Sweden)", "Thai (Thailand)", "Turkish (Turkey)"]
     
     var languageAccentCode : [String] = ["ar-SA", "zh-CN", "zh-HK", "zh-TW", "cs-CZ", "da-DK", "nl-BE", "nl-NL", "en-AU", "en-IE", "en-ZA", "en-GB", "en-US", "fi-FI", "fr-CA", "fr-FR", "de-DE", "el-GR", "he-IL", "hi-IN", "hu-HU", "id-ID", "it-IT", "ja-JP", "ko-KR", "no-NO", "pl-PL", "pt-BR", "pt-PT", "ro-RO", "ru-RU", "sk-SK", "es-MX", "es-ES", "sv-SE", "th-TH", "tr-TR"]
@@ -29,9 +28,7 @@ class ViewController: UIViewController, AVSpeechSynthesizerDelegate, UITextField
         
         accentPickerView.delegate = self
         accentPickerView.dataSource = self
-        
         speechSynthesizer.delegate = self
-        self.speechVoice = AVSpeechSynthesisVoice.init(language: "en-AU")
     }
     
     
@@ -75,8 +72,6 @@ class ViewController: UIViewController, AVSpeechSynthesizerDelegate, UITextField
     
     
     //UIPickerview delegate methods
-    
-    
     func numberOfComponents(in pickerView: UIPickerView) -> Int
     {
         return 1
@@ -95,9 +90,8 @@ class ViewController: UIViewController, AVSpeechSynthesizerDelegate, UITextField
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
     {
         languageAccentLabel.text = languageTitle[row]
+        self.speechVoice = AVSpeechSynthesisVoice.init(language: languageAccentCode[row])
     }
-    
-    
     
     
     func tap(_ gesture: UITapGestureRecognizer)
